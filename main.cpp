9c17740bc436159e1c98dc727f55b793b534c058
Geerten <g.j.w.verweij@tudelft.nl>
2026-02-12 21:32:52 +0100

    added a todo

--- main.cpp
+++ main.cpp
@@ -1,10 +1,6 @@
-// Dear ImGui: standalone example application for Windows API + DirectX 11
-
-// Learn about Dear ImGui:
-// - FAQ                  https://dearimgui.com/faq
-// - Getting Started      https://dearimgui.com/getting-started
-// - Documentation        https://dearimgui.com/docs (same as your local docs/ folder).
-// - Introduction, links and more at the top of imgui.cpp
+// todo:
+// next match moet winnaar kleur behouden.
+// bij cancel match moet vorige winnaar niet op queue.
 
 #include "imgui.h"
 #include "imgui_impl_win32.h"
