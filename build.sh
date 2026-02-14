04cb23e390e752fa82e3b46093d29862670b0f38
Losmoges <email@example.com>
2026-02-14 10:01:17 +0000

    polishing build.sh

--- build.sh
+++ build.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 set -o errexit
+cd "$(dirname "$0")"
 
 # --- Configuration ---
 OUT_DIR=build
-OUT_NAME=%OUT_DIR%\tabs_scoreboard.exe
+OUT_NAME=$OUT_DIR/tabs_scoreboard.exe
 IMGUI_DIR=./imgui
 BACKEND_DIR=./imgui/backends
 
 # Create the build directory if it doesn't exist
-if ! test -d $OUT_DIR; then mkdir $OUT_DIR; fi
+mkdir -p $OUT_DIR
 
 # --- Sync Assets ---
-# This copies everything from an 'assets' folder to your 'build' folder
-# /I creates the folder if it doesn't exist, /Y overwrites without asking
+# This copies everything from an 'assets' folder to your 'build/assets' folder
 if test -d "assets"; then
     echo Syncing assets...
     cp -r assets/* $OUT_DIR/assets/
@@ -26,9 +26,9 @@ clang++ \
     -std=c++17 \
     main.cpp \
     $IMGUI_DIR/*.cpp \
-    $BACKEND_DIR/imgui_impl_glfw.cpp \
-    $BACKEND_DIR/imgui_impl_opengl3.cpp \
-    -I$IMGUI_DIR \
-    -I$BACKEND_DIR \
+    "$BACKEND_DIR/imgui_impl_glfw.cpp" \
+    "$BACKEND_DIR/imgui_impl_opengl3.cpp" \
+    "-I$IMGUI_DIR" \
+    "-I$BACKEND_DIR" \
     -lglfw -lGL -ldl -lpthread -lX11 \
-    -o $OUT_NAME
+    -o "$OUT_NAME"
